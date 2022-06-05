2d32c957d07299050c255fa43cb757e00aed0599
whiskas009 <76867818+whiskas009@users.noreply.github.com>
2022-06-05 15:26:47 +0300

    #1

---
+++ Task_26_Array_Split/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_26_Array_Split
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string phrase = Console.ReadLine();
+            string[] wordInPhrase = phrase.Split(' ');
+
+            foreach (string word in wordInPhrase)
+                Console.WriteLine(word);
+        }
+    }
+}
